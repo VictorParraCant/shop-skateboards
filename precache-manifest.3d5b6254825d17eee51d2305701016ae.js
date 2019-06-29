fa3657866953f5df07251e06b4adb440d34325b8
VictorParraCant <victor.parracant@gmail.com>
2019-06-29 16:38:15 +0200

    Updates

---
+++ precache-manifest.3d5b6254825d17eee51d2305701016ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9caecaa093920af66695d11e27ac9bf6",
+    "url": "/shop-skateboards/index.html"
+  },
+  {
+    "revision": "f804fac280d8daec2ea0",
+    "url": "/shop-skateboards/static/css/main.f1a47e06.chunk.css"
+  },
+  {
+    "revision": "0e9b0520bc32582fe08a",
+    "url": "/shop-skateboards/static/js/2.dc9f490c.chunk.js"
+  },
+  {
+    "revision": "f804fac280d8daec2ea0",
+    "url": "/shop-skateboards/static/js/main.b345a4a7.chunk.js"
+  },
+  {
+    "revision": "659ff975022bc45741e6",
+    "url": "/shop-skateboards/static/js/runtime~main.f16c60b6.js"
+  }
+]);
