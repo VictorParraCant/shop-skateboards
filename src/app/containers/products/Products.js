8d69c8f1da26876806e27ed0ca417ce0766d357a
VictorParraCant <victor.parracant@gmail.com>
2019-06-29 14:26:16 +0200

    Show all info to products

--- src/app/containers/products/Products.js
+++ src/app/containers/products/Products.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 
+import BackgroundAnimated from '../../components/bgAnimated/BgAnimated';
 import Skate from '../../components/product/Skateboard';
+import Description from '../description/Description';
+import Shopcart from '../shopcart/Shopcart';
 
 import './products.scss';
 
@@ -24,13 +27,27 @@ class Products extends Component {
     return (
       <div className='container-page'>
         <div className='product-view'>
+          <BackgroundAnimated backgrounds={product.backgrounds}/>
           <Skate
             name={product.name}
-            thumbtail={product.thumbtail}
+            thumbnail={product.thumbnail}
           />
         </div>
         <div className='product-data'>
+          <Description
+              type={product.type}
+              name={product.name}
+              description={product.description}
+              properties={product.properties}
+              quantity={product.quantity}
+            />
 
+            <Shopcart
+              price={product.price}
+              handlerChange={e => this.handlerInputChange(e.target.value)}
+              handlerClick={this.handlerAddToCart}
+              animated={animated}
+            />
         </div>
       </div>
     );
